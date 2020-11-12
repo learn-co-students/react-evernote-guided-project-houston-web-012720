1c7dbd1d7b850df2eb0197099efae644644cf198
Placido Wang <placidow@gmail.com>
2020-11-12 03:31:07 -0600

    Done.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,6 +3,24 @@ import Header from './Header';
 import NoteContainer from './NoteContainer';
 
 class App extends Component {
+  state = {
+    notes: []
+  }
+
+  componentDidMount() {
+    console.log('fetching...');
+    fetch("http://localhost:3000/api/v1/notes")
+      .then(r => r.json())
+      .then(data => {
+        // console.log(data)
+        this.setState({notes: data});
+        // console.log(this.state)
+      }),
+      (error) => {
+
+      }
+  }
+
   render() {
     return (
       <div className="app">
